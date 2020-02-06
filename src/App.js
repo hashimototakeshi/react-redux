58f0914b1d4cf504cf599904dc169e398f4f2ba4
Hashimoto Takeshi <hashimoto@no1s.biz>
2020-02-06 03:59:10 +0000

    prop-types

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const App = () => {
 const profiles = [
 	{ name: "taro", age: 18 }
 	,{ name: "hanako", age: 5 }
-	,{ name: "kurose" }
+	//,{ name: "Yamada" } これはワーニング　isRequired
+	//,{ name: 18 }   これはワーニング string
 ]
 
 	return (
@@ -21,11 +23,10 @@ const profiles = [
 const User = (props) => {
 	return <div>Hi, i am {props.name}, and i am {props.age} years old!</div>
 }
-User.defaultProps = {
-	age: 1,
-	name: "taro"
+User.propTypes = {
+	name: PropTypes.string,
+	age: PropTypes.number.isRequired
 }
-
 
 export default App;
 
